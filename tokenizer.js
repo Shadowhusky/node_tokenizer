b3d1b89510a5525780865b23c7fe27c61f4d4106
Richard <a983974247@gmail.com>
2021-03-04 19:03:55 +0800

    feat: 🎸 Added jsdoc && Fixed num_words initial value

--- tokenizer.js
+++ tokenizer.js
@@ -1,7 +1,16 @@
 class Tokenizer {
+
+  /**
+   * Class Tokenizer
+   * @param filters a string where each element is a character that will be filtered from the texts. The default is all punctuation, plus tabs and line breaks, minus the ' character.
+   * @param num_words the maximum number of words to keep, based on word frequency. Only the most common num_words-1 words will be kept.
+   * @param oov_token if given, it will be added to word_index and used to replace out-of-vocabulary words during text_to_sequence calls
+   * @param lower boolean. Whether to convert the texts to lowercase.
+   * @return tokenizer object
+   */
   constructor(config = {}) {
     this.filters = config.filters || /[\\.,/#!$%^&*;:{}=\-_`~()]/g;
-    this.num_words = parseInt(config.num_words) || 0;
+    this.num_words = parseInt(config.num_words) || null;
     this.oov_token = config.oov_token || "";
     this.lower = typeof config.lower === "undefined" ? true : config.lower;
 
@@ -45,7 +54,8 @@ class Tokenizer {
     // Only translate the top num_words(if provided) of words.
     return texts.map((text) =>
       this.cleanText(text).flatMap((word) =>
-        this.word_index[word] && this.word_index[word] < this.num_words
+        this.word_index[word] &&
+        (this.num_words === null || this.word_index[word] < this.num_words)
           ? this.word_index[word]
           : this.oov_token
           ? 1
@@ -63,13 +73,17 @@ class Tokenizer {
   }
 }
 
-const tokenizerFromJson = (json_string) => {
+/**
+ * Create tokenizer from Json string
+ * @param json_string JSON string encoding a tokenizer configuration.
+ */
+function tokenizerFromJson(json_string) {
   const tokenizer = new Tokenizer();
   const js = JSON.parse(json_string);
   tokenizer.word_index = js.word_index;
   tokenizer.index_word = js.index_word;
   tokenizer.word_counts = js.word_counts;
   return tokenizer;
-};
+}
 
 module.exports = { Tokenizer, tokenizerFromJson };
